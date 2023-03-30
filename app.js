394ac7141bc4e1fc7412ce805d7c3700f8d0cb64
K Vivekananda Reddy <vivekananda-reddy@users.noreply.github.com>
2023-03-30 15:42:20 -0400

    completed a8

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import HelloControllers from "./controllers/hello-controllers.js";
 import UserController from "./controllers/users/users-controller.js";
+import TuitsController from "./controllers/tuits/tuits-controller.js";
+import cors from "cors";
 const app = express()
 app.use(express.json());
-
+app.use(cors());
+TuitsController(app);
 HelloControllers(app)
 UserController(app)
 app.listen(4000)
